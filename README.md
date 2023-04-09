5c086cccb4c1a8157eef257cb2e3ffab92cfbea7
Anthhon <anthcomercial@gmail.com>
2023-04-09 17:02:48 -0300

    bash script and documentation added

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# vicos
-Bash script for video conversion to .mp4 format with ffmpeg.
+# video-conversor-script
+This script converts video files of specific formats to the .mp4 format using the ffmpeg tool. It prompts the user to choose the output location for the converted file.
+
+## Usage
+To use the script, run it with the following command:
+```bash
+./convert_video.sh input_file [options]
+```
+The `input_file` argument is the path to the video file that you want to convert. The script requires at least one argument.
+
+The following options are available:
+-   `-v` or `--verbose`: prints the ffmpeg output to the console.
+
+## Supported formats
+The following formats are supported as input formats:
+
+-   .webm
+-   .mkv
+-   .gif
+-   .mov
+-   .avi
+-   .wmv
+-   .flv
+-   .vob
+-   .gifv
+-   .avi
+-   .amv
+
+The output format is always .mp4. And can be changed in the line 4 of the code:
+```bash
+  4 output_format=.mp4
+```
+
+## Output location
+
+The script prompts the user to choose the output location for the converted file. The available options are:
+
+-   Home
+-   Desktop
+-   Downloads
+-   Current folder
+
+## Example
+```bash
+./convert_video.sh input.webm -v
+```
+This command converts the `input.webm` file to .mp4 format and prints the ffmpeg output to the console.
+
+## Requirements
+The script requires the `ffmpeg` tool to be installed on the system.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](/blob/main/LICENSE) file for details.
---
+++ src/vicos.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+source_format=(.webm .mkv .gif .mov .avi .wmv .flv .vob .gifv .avi .amv)
+output_format=.mp4
+verbose_flag=0
+
+# Check if input file has been provided as an argument
+if [ -z "$1" ]; then
+	echo "ERROR: Input file not provided"
+	return 1
+fi
+echo "File name: $1"
+
+# Check if the input file exists and has the correct format
+input_file=$1
+input_format=${input_file##*.}
+echo "File format: $input_format"
+
+if ! [[ "${source_format[@]}" =~ "${input_format}" ]]; then
+    echo "ERROR: Input file format must be a video format ${source_formats[@]}"
+    return 1
+elif [ ! -f "$input_file" ]; then
+	echo "ERROR: Input file doesn't exist"
+	return 1
+fi
+echo "File found!"
+
+# Check for verbose flag
+if [[ "$2" == "-v" || "$2" == "--verbose" ]] ; then
+	echo "Verbose flag provided"
+	verbose_flag=1
+fi
+
+# Set source and output video names
+source_video=$input_file
+output_name="${source_video%.*}$output_format"
+
+# Ask the user for the output path
+echo "Where do you want to save the converted file?"
+PS3="Please enter your choice: "
+options=("Home" "Desktop" "Downloads" "Current folder")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Home")
+            output_path="$HOME/$output_name"
+            break
+            ;;
+        "Desktop")
+            output_path="$HOME/Desktop/$output_name"
+            break
+            ;;
+        "Downloads")
+            output_path="$HOME/Downloads/$output_name"
+            break
+            ;;
+        "Current folder")
+            output_path="./$output_name"
+            break
+            ;;
+        *) echo "Invalid option. Please enter a valid number.";;
+    esac
+done
+
+# Convert the input to the output format using ffmpeg
+echo "Converting file..."
+if [[ $verbose_flag -eq 1 ]]; then
+	ffmpeg -i "$source_video" "$output_path"
+else
+	ffmpeg -i "$source_video" "$output_path" > /dev/null 2>&1
+fi
+
+# Success message outputting the file path
+echo "File saved as $output_path"
